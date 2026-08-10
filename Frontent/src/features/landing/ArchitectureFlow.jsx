41e144d86d09d203aa85937fbdf790ed51aeaa3c
Sakshi Gupta <sakshigupta182226@gmail.com>
2026-08-10 18:46:34 +0530

    Remove node_modules from repository

--- Frontent/src/features/landing/ArchitectureFlow.jsx
+++ Frontent/src/features/landing/ArchitectureFlow.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const ArchitectureFlow = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default ArchitectureFlow
